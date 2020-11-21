c4ff35fc4287cf1ffba5adefd4e6bef75b2dae5c
c4-dawson <christian.ecdawson@gmail.com>
2020-11-21 14:28:47 +0000

    styled components - no need for app.css

--- src/App.js
+++ src/App.js
@@ -42,38 +42,39 @@ function App() {
   }
 
   return (
-    <div className = "App"/*css={main}*/>
-      <header className = "App-header"/*css={main_header}*/>
-        <h1>Badge component</h1>
-        <PrimaryBadge>Value</PrimaryBadge>
-        <Badge>Default</Badge>
-        <Badge variantColor="green">Green</Badge>
-        <Badge variantColor="red">Red</Badge>
-        <Badge variantColor="yellow" color = "#444">Yellow</Badge>
-        <CreateFilmForm onSubmit={onSubmit} />
-        <SearchFilmForm onTitleChange = {handleTitleChange} />
-        {!isEmpty(films) && (
-          <ul id = "films">
-            {films.map(renderFilm)}
-          </ul>
-        )}
-      </header>
-    </div>
+    <App_Main>
+
+      <Button_Header>Button component</Button_Header>
+
+      <Button_Container row>
+        <Button as = "a" 
+          href = "https://www.linkedin.com/in/christian-dawson-674b841b6/">
+          Linked<a>In</a></Button>
+        <Button primary>Value</Button>
+        <Button color="green">Green</Button>
+        <Button color="red">Red</Button>
+        <Button primary color="yellow">Yellow</Button>
+      </Button_Container>
+
+      <Form_Container row>
+        <CreateFilmForm css = {Create_Film_Form} onSubmit={onSubmit} />
+        <SearchFilmForm css = {Search_Film_Form} onTitleChange = {handleTitleChange} />
+      </Form_Container>
+
+      {!isEmpty(films) && (
+        <ul id = "films">
+          {films.map(renderFilm)}
+        </ul>
+      )}
+
+    </App_Main>
   );
   
 }
 
 export default App;
 
-
-/*
-const main = css`
-  text-align: center;
-  display: felx;
-  background-color: #282c34;
-`;
-
-const main_header = css`
+const App_Main = styled.div`
   background-color: #282c34;
   min-height: 100vh;
   display: flex;
@@ -82,22 +83,45 @@ const main_header = css`
   justify-content: center;
   font-size: calc(10px + 2vmin);
   color: white;
-`;
-*/
+`
 
-const Badge = styled.span`
-    background-color: ${props =>
-       props.variantColor ? props.variantColor: 'grey' };
-    color: ${props => props.color ? props.color : "#fff"};
-    padding: 10px;
-    border-radius: 3px;
-    margin: 10px;
-  `;
-
-const PrimaryBadge = styled(Badge)`
-  background-color: #ddd;
-  color: #444;
+const Button_Header = styled.header`
+  color : hotpink;
+  font-size: 50px;
 `;
+
+const Button_Container = styled.div`(props => ({
+  display: 'flex',
+  flexDirection: props.row && 'row'
+}))`;
+
+const Button = styled.button`
+  background-color: ${props =>
+      props.color ? props.color: 'grey' };
+  color: ${props =>
+      props.primary ? 'black' : 'white'};
+    & > a {
+      color: 'hotpink';
+    }  
+  padding: 10px;
+  border-radius: 3px;
+  margin: 10px;
+  font-size: 50px;
+`;
+
+const Form_Container = styled.div`(props => ({
+  display: 'flex',
+  flexDirection: props.row && 'row'
+}))`;
+
+const Create_Film_Form = styled.form`
+  font-size: 50px;
+`;
+
+const Search_Film_Form = styled.form`
+  font-size: 50px;
+`;
+
 
 /* Theming
 - extract the two forms into separate components
