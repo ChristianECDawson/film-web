d480b48ccb839e665bd28e91517ea18bae0464ca
c4-dawson <christian.ecdawson@gmail.com>
2020-11-25 12:33:38 +0000

    passing grid-area through props?

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,9 +1,14 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
 import { isNil } from "lodash";
+import CreateFilmForm from "./CreateFilmForm";
+import SearchFilmForm from "./SearchFilmForm";
+import Button from "./Button";
+import FilmList from './FilmList';
+import Header from './Header';
 
 function Grid(props) {
-    const {children} = props;
+    const {children, Header, Button, FilmList, SearchFilmForm, CreateFilmForm} = props;
 
     return (
         <div style = {css`
@@ -11,8 +16,8 @@ function Grid(props) {
             grid-template-columns: 1fr 1fr 1fr 1fr
             grid-template-rows: auto;
             grid-template-areas:
-                "header header nav nav"
-                "main main . sidebar"
+                "${Header} ${Header} ${Button} ${Button}"
+                "${FilmList} . ${SearchFilmForm} ${CreateFilmForm}"
                 "footer footer footer footer";
         `}>
         {children}    
